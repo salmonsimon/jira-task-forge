d4bad986e92e326a0c627190a2309c065b4a0505
Saimon Bahamonde <saimon@local>
2026-05-20 17:20:50 -0400

    Style native select options for dark theme

--- src/features/trays/QuickCapture.tsx
+++ src/features/trays/QuickCapture.tsx
@@ -74,7 +74,7 @@ function CaptureSelect({
   return (
     <select
       aria-label={ariaLabel}
-      className={`${width} h-9 rounded border border-[#c1c7d0] bg-white px-3 text-sm outline-none focus:border-[#4c9aff] focus:ring-2 focus:ring-[#deebff] dark:border-[#5c606a] dark:bg-[#303238] dark:text-[#f4f5f7] dark:focus:border-[#85b8ff] dark:focus:ring-[#1d355c]`}
+      className={`${width} h-9 rounded border border-[#c1c7d0] bg-white px-3 text-sm outline-none focus:border-[#4c9aff] focus:ring-2 focus:ring-[#deebff] [&_option]:bg-white [&_option]:text-[#172b4d] dark:border-[#5c606a] dark:bg-[#303238] dark:text-[#f4f5f7] dark:focus:border-[#85b8ff] dark:focus:ring-[#1d355c] dark:[&_option]:bg-[#303238] dark:[&_option]:text-[#f4f5f7]`}
       value={value}
       onChange={(event) => onChange(event.target.value)}
     >
